1e0d21874be60bc363e5b8032bf0ba0453cc99e2
elyasalsaffar <elyas.dusty@gmail.com>
2025-06-24 21:39:09 +0300

    initializing backend

---
+++ routes/AuthRouter.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const controller = require('../controllers/AuthController')
+const middleware = require('../middleware')
+
+router.put(
+  '/update/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+)
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
+
+module.exports = router
