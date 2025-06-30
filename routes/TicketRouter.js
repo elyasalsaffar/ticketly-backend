89e4c062c055d805d398180f5784188d6adbbf29
HasanJuma <hasanjuma2022@email.com>
2025-06-30 19:26:59 +0300

    remove hte extra TicketController

--- routes/TicketRouter.js
+++ routes/TicketRouter.js
@@ -1,23 +1,9 @@
+const express = require('express')
 const router = require('express').Router()
-const middleware = require('../middleware')
-const TicketController = require('../controllers/TicketController')
+const { CreateTicket } = require('../controllers/TicketController')
+const verifyToken = require('../middleware/verifyToken')
 
-// Get all tickets for the user
-router.get(
-  '/tickets',
-  middleware.stripToken,
-  middleware.verifyToken,
-  TicketController.getUserTickets
-)
 
-// Get a specific ticket by ID for the user
-router.get(
-  '/tickets/:id',
-  middleware.stripToken,
-  middleware.verifyToken,
-  TicketController.getTicketById
-)
+router.post('/', verifyToken, CreateTicket)
 
 module.exports = router
-
-
